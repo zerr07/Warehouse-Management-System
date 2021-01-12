da350e930783cdc1c8d6d10cd8a83636e0bb4498
zerr07 <zajevv@gmail.com>
2021-01-12 17:20:36 +0200

    New api for reservations implemented

--- cp/POS/reserve/reserve.php
+++ cp/POS/reserve/reserve.php
@@ -35,32 +35,34 @@ function reserveCart($note, $cart){
         if ($type == "2" || $type == 2){
             $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */"INSERT INTO {*shipment_status*} (id_status, id_shipment) VALUES ('1', '$id')"));
         }
-        foreach ($cart as $key => $value){
-
-                $quantity = $value['quantity'];
-                $price = $value['price'];
-                $basePrice = $value['basePrice'];
-                $id_loc = "";
-                if (isset($value['loc']['selected'])){
-                    $id_loc = $value['loc']['selected'];
-                } elseif (isset($value['id_location'])){
-                    $id_loc = $value['id_location'];
-                }
-                if($value['tag'] != "Buffertoode") {
-                    update_quantity($key, $id_loc, "-", $quantity);
-                } else {
-                    $key = $value['name'];
-                }
-
-                mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */
-                    "INSERT INTO {*reserved_products*} (id_reserved, id_product, price, quantity, basePrice, id_location
-                                        ) VALUES ('$id', '$key', '$price', '$quantity', '$basePrice', '$id_loc')"));
-
-        }
+        addCartToReservation($id, $cart);
     }
     updateCart();
 }
+function addCartToReservation($id, $cart){
+    foreach ($cart as $key => $value){
 
+        $quantity = $value['quantity'];
+        $price = $value['price'];
+        $basePrice = $value['basePrice'];
+        $id_loc = "";
+        if (isset($value['loc']['selected'])){
+            $id_loc = $value['loc']['selected'];
+        } elseif (isset($value['id_location'])){
+            $id_loc = $value['id_location'];
+        }
+        if($value['tag'] != "Buffertoode") {
+            update_quantity($key, $id_loc, "-", $quantity);
+        } else {
+            $key = $value['name'];
+        }
+
+        mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */
+            "INSERT INTO {*reserved_products*} (id_reserved, id_product, price, quantity, basePrice, id_location
+                                        ) VALUES ('$id', '$key', '$price', '$quantity', '$basePrice', '$id_loc')"));
+
+    }
+}
 function reserveCartWithoutUpdate($note, $cart){
     if (isset($_GET['type'])){
         $type = $_GET['type'];
@@ -76,28 +78,7 @@ function reserveCartWithoutUpdate($note, $cart){
         if ($type == "2" || $type == 2){
             $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */"INSERT INTO {*shipment_status*} (id_status, id_shipment) VALUES ('1', '$id')"));
         }
-        foreach ($cart as $key => $value){
-
-            $quantity = $value['quantity'];
-            $price = $value['price'];
-            $basePrice = $value['basePrice'];
-            $id_loc = "";
-            if (isset($value['loc']['selected'])){
-                $id_loc = $value['loc']['selected'];
-            } elseif (isset($value['id_location'])){
-                $id_loc = $value['id_location'];
-            }
-            if($value['tag'] != "Buffertoode") {
-                update_quantity($key, $id_loc, "-", $quantity);
-            } else {
-                $key = $value['name'];
-            }
-
-            mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */
-                "INSERT INTO {*reserved_products*} (id_reserved, id_product, price, quantity, basePrice, id_location
-                                        ) VALUES ('$id', '$key', '$price', '$quantity', '$basePrice', '$id_loc')"));
-
-        }
+        addCartToReservation($id, $cart);
     }
 }
 function getReservedCartsSearch($type){
@@ -175,16 +156,37 @@ function getReservationsDatalist($type){
     return array_filter($arr);
 }
 
-function getReservedCarts($type){
+function getReservedCarts($type='unset', $api=false){
     $arr = array(array());
     $str = "";
     if ($type == 2){
         $str = " AND (SELECT id_status FROM {*shipment_status*} WHERE id_shipment={*reserved*}.id)!='6'";
     }
-    $q = mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */ "SELECT * FROM {*reserved*} WHERE id_type='$type' $str ORDER BY date DESC"));
+    $type_q = "WHERE id_type='$type'";
+    if ($type == 'unset'){
+        $type_q = "WHERE id_type='1' OR id_type='2'";
+    }
+    if ($api){
+        if ($type == 'unset'){
+            $q = mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */ "SELECT id, comment, id_type FROM {*reserved*} $type_q $str ORDER BY date DESC"));
+        } else {
+            $q = mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */ "SELECT id, comment FROM {*reserved*} $type_q $str ORDER BY date DESC"));
+
+        }
+    } else {
+        $q = mysqli_query($GLOBALS['DBCONN'], prefixQuery(/** @lang text */ "SELECT * FROM {*reserved*} $type_q $str ORDER BY date DESC"));
+    }
     while ($row = mysqli_fetch_assoc($q)){
-        $id = $row['id'];
-        $arr[$id] = readReservationResult($row);
+        if ($api){
+            $arr[$row['id']]['comment'] = $row['comment'];
+            if ($type == 'unset'){
+                $arr[$row['id']]['id_type'] = $row['id_type'];
+            }
+        } else {
+            $id = $row['id'];
+            $arr[$id] = readReservationResult($row);
+        }
+
     }
     return array_filter($arr);
 }
@@ -271,6 +273,91 @@ function cancelReservationProduct($id, $id_prod_res){
         }
     }
 }
+function editReservation($id, $data){
+    $oldData = getSingleCartReservation($id);
+    foreach ($data as $value){
+        if (isset($value['tag'])){
+            $found = false;
+            foreach ($oldData['products'] as $val){
+                if ($val['tag'] == $value['tag']){
+                    $value['id'] = $val['id'];
+                    if (isset($value['quantity'])){
+                        editReservationProduct($value, $val);
+                        $found = true;
+                    } else {
+                        exit(json_encode(array("error"=>"No quantity field supplied.", "code"=>"200")));
+                    }
+                }
+            }
+            if (!$found){
+                include_once($_SERVER['DOCUMENT_ROOT'] . "/api/func/fromCart.php");
+                $cart = formCart($value['tag'], array_filter($value));
+                addCartToReservation($id, array_filter($cart));
+            }
+        } else if (isset($value['id'])){
+            foreach ($oldData['products'] as $val){
+                if ($val['id'] == $value['id']){
+                    if (isset($value['quantity'])){
+                        editReservationProduct($value, $val);
+                    } else {
+                        exit(json_encode(array("error"=>"No quantity field supplied.", "code"=>"200")));
+                    }
+                }
+            }
+        }
+    }
+    return $oldData;
+}
+
+function editReservationProduct($value, $oldValues){
+    $id = $value['id'];
+    $quantity = $value['quantity'];
+    if (isset($value['price']) && isset($value['basePrice'])){
+        $price = round($value['price'], 2);
+        $basePrice = round($value['basePrice'], 2);
+    } elseif (isset($value['basePrice'])){
+        $basePrice = round($value['basePrice'], 2);
+        $price = round($quantity*$basePrice, 2);
+    } elseif (isset($value['price'])){
+        $price = round($value['price'], 2);
+        $basePrice = round($price/$quantity, 2);
+    } else {
+        $price = round($oldValues['basePrice']*$quantity, 2);
+        $basePrice = round($oldValues['basePrice'], 2);
+    }
+
+    if (!is_numeric($price) || !is_numeric($basePrice)){
+        exit(json_encode(array("error"=>"Price or Base price is not number.", "code"=>"201")));
+    }
+    $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "UPDATE {*reserved_products*} SET `price`='$price', basePrice='$basePrice', quantity='$quantity' WHERE id='$id'"));
+    if (!$q){
+        echo "SQL error $id";
+    }
+    updateProductPriceInReservationEdit($oldValues, $quantity);
+}
+
+function updateProductPriceInReservationEdit($value, $newQuantity){
+    $id_location = $value['id_location'];
+    $id_product = $value['id_product'];
+    if ($id_product != "Buffertoode"){
+        if ($value['id_location'] !== 0 || $value['id_location'] !== null){
+            $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "SELECT * FROM {*product_locations*} WHERE id='$id_location'"));
+            $row_q = $q->fetch_assoc();
+            $quantity = $row_q['quantity']+$value['quantity']-$newQuantity;
+            $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "UPDATE {*product_locations*} SET quantity='$quantity' WHERE id='$id_location'"));
+        } else {
+            $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "SELECT * FROM {*product_locations*} WHERE id_product='$id_product' LIMIT 1"));
+            $row_q = $q->fetch_assoc();
+            $quantity = $row_q['quantity']+$value['quantity']-$newQuantity;
+            $new_loc_id = $row_q['id'];
+            $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "UPDATE {*product_locations*} SET quantity='$quantity' WHERE id='$new_loc_id'"));
+        }
+        //PR_PUT_Product($id_product);
+    }
+
+}
+
+
 $post=json_decode(file_get_contents("php://input"));
 
 if (isset($post->req)){
