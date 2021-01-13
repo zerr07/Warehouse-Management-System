b8da1ebe7d2a1e5d375b02d31ae25307409ebe51
zerr07 <zajevv@gmail.com>
2021-01-13 12:14:12 +0200

    Reservations API routes moved to separate file

--- api/index.php
+++ api/index.php
@@ -2,6 +2,6 @@
 include_once($_SERVER['DOCUMENT_ROOT'].'/api/Routing.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/api/checkToken.php');
 
-include_once($_SERVER['DOCUMENT_ROOT'].'/api/routes.php');
+include_once($_SERVER['DOCUMENT_ROOT'].'/api/routes/reservations.php');
 include_once($_SERVER['DOCUMENT_ROOT'].'/api/FB/routes.php');
 Route::run('/');
