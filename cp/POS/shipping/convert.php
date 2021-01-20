9789a23e0edbaf25163c402814f9cb2a5bfe6115
zerr07 <zerrx07@gmail.com>
2021-01-20 16:39:58 +0200

    Added check if status already set

--- cp/POS/shipping/convert.php
+++ cp/POS/shipping/convert.php
@@ -3,7 +3,12 @@ include($_SERVER["DOCUMENT_ROOT"].'/configs/config.php');
 
 function convertToShipping($id){
     $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */"UPDATE {*reserved*} SET id_type='2' WHERE id='$id'"));
-    $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */"INSERT INTO {*shipment_status*} (id_status, id_shipment) VALUES ('1', '$id')"));
+    $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang text */ "SELECT id FROM {*shipment_status*} WHERE id_shipment='$id'"));
+    if ($q){
+        if ($q->num_rows == 0){
+            $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */"INSERT INTO {*shipment_status*} (id_status, id_shipment) VALUES ('1', '$id')"));
+        }
+    }
 
     return json_encode(array("response"=>"success"));
 }
