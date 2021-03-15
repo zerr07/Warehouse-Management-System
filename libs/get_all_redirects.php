efde528476d54802987faee73f66ea498bbeda7d
zerr07 <zerrx07@gmail.com>
2021-03-15 11:05:20 +0200

    Libraries with no repository added

---
+++ libs/get_all_redirects.php
@@ -0,0 +1,13 @@
+<?php
+function getRedirectUrl ($url) {
+    stream_context_set_default(array(
+        'http' => array(
+            'method' => 'HEAD'
+        )
+    ));
+    $headers = get_headers($url, 1);
+    if ($headers !== false && isset($headers['Location'])) {
+        return $headers['Location'];
+    }
+    return false;
+}
