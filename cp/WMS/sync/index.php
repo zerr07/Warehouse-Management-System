e333b570ecaa75e844c7373eb9e88d68c3175267
zerr07 <zerrx07@gmail.com>
2021-03-15 11:04:48 +0200

    Sync query changed to use id from config

--- cp/WMS/sync/index.php
+++ cp/WMS/sync/index.php
@@ -16,8 +16,8 @@ include_once($_SERVER["DOCUMENT_ROOT"]).'/controllers/prestashop/Products.php';
 //$smarty->assign("products", get_products($_COOKIE['id_shard']));
 if (isset($_GET['getProducts'])){
     $arr = array();
-    $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "SELECT id, tag, (SELECT export FROM {*product_platforms*} WHERE id_item={*products*}.id AND id_platform=2) as active
-    FROM {*products*} WHERE (SELECT COUNT(*) FROM {*product_platforms*} WHERE id_item={*products*}.id AND id_platform=2 and export=1)!=0"));
+    $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "SELECT id, tag, (SELECT export FROM {*product_platforms*} WHERE id_item={*products*}.id AND id_platform="._ENGINE['ps_platform_id'].") as active
+    FROM {*products*} WHERE (SELECT COUNT(*) FROM {*product_platforms*} WHERE id_item={*products*}.id AND id_platform="._ENGINE['ps_platform_id']." and export=1)!=0"));
     while ($row = $q->fetch_assoc()){
         $arr[$row['tag']] = array("active"=>$row['active'], "quantity" => get_quantity_sum($row['id']));
     }
@@ -42,6 +42,7 @@ if (isset($_GET['getProducts'])){
     $q = $GLOBALS['DBCONN']->query(prefixQuery(/** @lang */ "SELECT id FROM {*products*} WHERE tag='$tag' LIMIT 1"));
     $id = $q->fetch_assoc()['id'];
     PR_PUT_Product_Without_IMG($id);
+    //PR_PUT_Product($id);
     exit("PUTPROD " . $id);
 } elseif (isset($_GET['DELETEPROD'])){
     PR_DELETE_Product_By_Tag($_GET['DELETEPROD']);
