202a682f56672353063690f75b9d292b31839e57
zerr07 <zerrx07@gmail.com>
2021-04-26 16:29:00 +0300

    Fixed location operations without sync

--- controllers/products/deleteLoc.php
+++ controllers/products/deleteLoc.php
@@ -1,10 +1,13 @@
 <?php
 
 include($_SERVER["DOCUMENT_ROOT"] . '/configs/config.php');
+include_once($_SERVER["DOCUMENT_ROOT"].'/controllers/prestashop/Products.php');
 
 function delete_location($id)
 {
     $GLOBALS['DBCONN']->query(prefixQuery(/** @lang text */ "DELETE FROM {*product_locations*} WHERE id='$id'"));
+    PR_PUT_Product($id);
+
 }
 
 if (isset($_GET['id'])) {
--- controllers/products/setLocation.php
+++ controllers/products/setLocation.php
@@ -1,14 +1,18 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"].'/configs/config.php');
+include_once($_SERVER["DOCUMENT_ROOT"].'/controllers/prestashop/Products.php');
+
 if (isset($_GET['id']) && isset($_GET['location'])){
     $id = $_GET['id'];
     $loc = $_GET['location'];
     if (isset($_COOKIE['default_location_type'])){
         $id_type = $_COOKIE['default_location_type'];
     } else {
-        $id_type = 1;
+        $id_type = 2;
     }
 
     $GLOBALS['DBCONN']->query(prefixQuery(/** @lang text */ "INSERT INTO {*product_locations*}
                                           (id_item, location, id_type) VALUES ('$id', '$loc', '$id_type')"));
+    PR_PUT_Product($id);
+
 }
