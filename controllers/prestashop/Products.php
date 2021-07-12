9a6561997198c62182e9bc1b10b6c4f7ab4c7893
zerr07 <zerrx07@gmail.com>
2021-07-12 15:42:30 +0300

    Ean codes and manufacturer added

--- controllers/prestashop/Products.php
+++ controllers/prestashop/Products.php
@@ -110,6 +110,11 @@ function PR_POST_Product_Image($id, $image, $json = true){
 function PR_POST_Product($id, $images=false){
     global $domain, $api_key;
     $data = get_product($id);
+    $codes = get_ean_codes($id);
+    $codes_xml = "";
+    if (!empty($codes)){
+        $codes_xml = "<ean13>".reset($codes)['ean']."</ean13>";
+    }
     $PriceTaxExcluded = round($data['platforms'][_ENGINE['ps_platform_id']]['price']/1.2, 5);
     $active = 0;
     if (isset($data['platforms'][_ENGINE['ps_platform_id']]['export'])){
@@ -148,9 +153,16 @@ function PR_POST_Product($id, $images=false){
             }
         }
     }
+    if (isset($data['id_manufacturer']) && $data['id_manufacturer'] != "" && !is_null(isset($data['id_manufacturer']))){
+        $manufacturer = '<id_manufacturer><![CDATA['.PR_GET_Manufacturer($data['id_manufacturer']).']]></id_manufacturer>';
+    } else {
+        $manufacturer = "";
+    }
     $xml = '
 <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
   <product>
+    '.$manufacturer.'
+    '.$codes_xml.'
     <name>
         <language id="'._LANG['ps_en'].'"><![CDATA['.trim($data['name']['en']).']]></language>
         <language id="'._LANG['ps_et'].'"><![CDATA['.trim($data['name']['et']).']]></language>
@@ -167,7 +179,6 @@ function PR_POST_Product($id, $images=false){
         <language id="'._LANG['ps_ru'].'"><![CDATA['.get_RU_URL(trim($data['name']['ru'])).']]></language>
     </link_rewrite>
     <available_for_order>'.$active.'</available_for_order>
-    
     <price>'.$PriceTaxExcluded.'</price>
     <reference>'.$data['tag'].'</reference>
     <active>'.$active.'</active>
@@ -175,6 +186,7 @@ function PR_POST_Product($id, $images=false){
     <pack_stock_type>3</pack_stock_type>
     <state>1</state>
     <indexed>1</indexed>
+    <minimal_quantity>1</minimal_quantity>
     <id_category_default>'.$data['main_category'].'</id_category_default>
     <id_tax_rules_group>1</id_tax_rules_group>
     <associations>
@@ -206,9 +218,18 @@ function PR_POST_Product($id, $images=false){
     }
 }
 
-function PR_PUT_Product($id_product, $images=false){
+function PR_PUT_Product($id_product, $images_upload=false){
     global $domain, $api_key;
     $data = get_product($id_product);
+    $codes = get_ean_codes($id_product);
+    $codes_xml = "";
+    if (!empty($codes)){
+        $codes_xml = "<ean13>".reset($codes)['ean']."</ean13>";
+    }
+    $pr = PR_GET_Product_By_Tag($data['tag']);
+    if (empty($pr)){
+        PR_POST_Product($id_product, true);
+    }
     $id = PR_GET_Product_By_Tag($data['tag'])['products'][0]['id'];
     $PriceTaxExcluded = round($data['platforms'][_ENGINE['ps_platform_id']]['price']/1.2, 5);
     $active = 0;
@@ -219,6 +240,108 @@ function PR_PUT_Product($id_product, $images=false){
     foreach ($data['categories'] as $value){
         $categories .= "<category><id>$value</id></category>";
     }
+    foreach ($data['name'] as $key => $value){
+        if ($value == ""){
+            foreach ($data['name'] as $name){
+                if ($name != ""){
+                    $data['name'][$key] = $name." ".$key;
+                }
+            }
+        }
+    }
+    foreach ($data['descriptions'] as $key => $value){
+        if ($value == ""){
+            foreach ($data['descriptions'] as $descriptions){
+                if ($name != ""){
+                    $data['descriptions'][$key] = $descriptions;
+                }
+            }
+        }
+    }
+    if (isset($data['id_manufacturer']) && $data['id_manufacturer'] != "" && !is_null(isset($data['id_manufacturer']))){
+        $manufacturer = '<id_manufacturer><![CDATA['.PR_GET_Manufacturer($data['id_manufacturer']).']]></id_manufacturer>';
+    } else {
+        $manufacturer = "";
+    }
+    $xml = '
+<prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+  <product>
+    <id>'.$id.'</id>
+    '.$manufacturer.'
+    '.$codes_xml.'
+    <name>
+        <language id="'._LANG['ps_en'].'"><![CDATA['.trim($data['name']['en']).']]></language>
+        <language id="'._LANG['ps_et'].'"><![CDATA['.trim($data['name']['et']).']]></language>
+        <language id="'._LANG['ps_ru'].'"><![CDATA['.trim($data['name']['ru']).']]></language>
+    </name>
+    <description>
+        <language id="'._LANG['ps_en'].'"><![CDATA['.trim($data['descriptions']['en']).']]></language>
+        <language id="'._LANG['ps_et'].'"><![CDATA['.trim($data['descriptions']['et']).']]></language>
+        <language id="'._LANG['ps_ru'].'"><![CDATA['.trim($data['descriptions']['ru']).']]></language>
+    </description>
+    <link_rewrite>
+        <language id="'._LANG['ps_en'].'"><![CDATA['.get_EN_URL(trim($data['name']['en'])).']]></language>
+        <language id="'._LANG['ps_et'].'"><![CDATA['.get_ET_URL(trim($data['name']['et'])).']]></language>
+        <language id="'._LANG['ps_ru'].'"><![CDATA['.get_RU_URL(trim($data['name']['ru'])).']]></language>
+    </link_rewrite>
+    <available_for_order>'.$active.'</available_for_order>
+    <price>'.$PriceTaxExcluded.'</price>
+    <reference>'.$data['tag'].'</reference>
+    <active>'.$active.'</active>
+    <show_price>1</show_price>
+    <minimal_quantity>1</minimal_quantity>
+    <pack_stock_type>3</pack_stock_type>
+    <state>1</state>
+    <indexed>1</indexed>
+    <id_category_default>'.$data['main_category'].'</id_category_default>
+    <id_tax_rules_group>1</id_tax_rules_group>
+    <associations>
+      <categories>
+        '.$categories.'
+      </categories>
+     </associations>
+  </product>
+</prestashop>';
+    $url = "https://$api_key@$domain/api/products?output_format=JSON";
+    $res = CallPUTAPI($url, $xml);
+    if (isset($res['product'])){
+        $id = $res['product']['id'];
+
+
+        $GLOBALS['BIGCONN']->query(prefixQuery(/** @lang */ "DELETE FROM {*ps_product_carrier*} WHERE id_product='$id'"));
+        foreach ($data['carrier'] as $val){
+            $carr_ref = $val['shop_id'];
+            if ($val['enabled'] == 1)
+                $GLOBALS['BIGCONN']->query(prefixQuery(/** @lang */ "INSERT INTO {*ps_product_carrier*} (id_product, id_carrier_reference, id_shop) VALUES ('$id', '$carr_ref', '1')"));
+        }
+        if ($images_upload){
+            $images = PR_GET_Product_Images($id);
+            if (!is_null($images)){
+                foreach ($images as $val){
+                    PR_DELETE_Product_Image($id, $val['id']);
+                }
+            }
+            foreach ($data['images'] as $key => $value){
+                $res = PR_POST_Product_Image($id, $_SERVER['DOCUMENT_ROOT']."/uploads/images/products/".$value['image'], false);
+                $id_img_prestashop = (string) simplexml_load_string($res)->image->id;
+                $id_img = $value['id'];
+                $GLOBALS['DBCONN']->query(prefixQuery(/** @lang text */ "UPDATE {*product_images*} SET id_prestashop='$id_img_prestashop' WHERE id='$id_img'"));
+            }
+        }
+
+        PR_PUT_Product_Stock_Available($id, $data['quantity']);
+    }
+}
+function PR_PUT_Product_only_desc($id_product){
+    global $domain, $api_key;
+    $data = get_product($id_product);
+    $pr = PR_GET_Product_By_Tag($data['tag']);
+    if (empty($pr)){
+        PR_POST_Product($id_product, true);
+    }
+    $PriceTaxExcluded = round($data['platforms'][_ENGINE['ps_platform_id']]['price']/1.2, 5);
+
+    $id = PR_GET_Product_By_Tag($data['tag'])['products'][0]['id'];
     foreach ($data['name'] as $key => $value){
         if ($value == ""){
             foreach ($data['name'] as $name){
@@ -256,55 +379,33 @@ function PR_PUT_Product($id_product, $images=false){
         <language id="'._LANG['ps_et'].'"><![CDATA['.get_ET_URL(trim($data['name']['et'])).']]></language>
         <language id="'._LANG['ps_ru'].'"><![CDATA['.get_RU_URL(trim($data['name']['ru'])).']]></language>
     </link_rewrite>
-    <available_for_order>'.$active.'</available_for_order>
     <price>'.$PriceTaxExcluded.'</price>
     <reference>'.$data['tag'].'</reference>
-    <active>'.$active.'</active>
-    <show_price>1</show_price>
-    <pack_stock_type>3</pack_stock_type>
-    <state>1</state>
-    <indexed>1</indexed>
-    <id_category_default>'.$data['main_category'].'</id_category_default>
-    <id_tax_rules_group>1</id_tax_rules_group>
-    <associations>
-      <categories>
-        '.$categories.'
-      </categories>
-     </associations>
   </product>
 </prestashop>';
+
     $url = "https://$api_key@$domain/api/products?output_format=JSON";
     $res = CallPUTAPI($url, $xml);
-    if (isset($res['product'])){
-        $id = $res['product']['id'];
-        $images = PR_GET_Product_Images($id);
-        if (!is_null($images)){
-            foreach ($images as $val){
-                PR_DELETE_Product_Image($id, $val['id']);
-            }
-        }
+}
 
-        $GLOBALS['BIGCONN']->query(prefixQuery(/** @lang */ "DELETE FROM {*ps_product_carrier*} WHERE id_product='$id'"));
-        foreach ($data['carrier'] as $val){
-            $carr_ref = $val['shop_id'];
-            if ($val['enabled'] == 1)
-                $GLOBALS['BIGCONN']->query(prefixQuery(/** @lang */ "INSERT INTO {*ps_product_carrier*} (id_product, id_carrier_reference, id_shop) VALUES ('$id', '$carr_ref', '1')"));
-        }
-        if ($images){
-            foreach ($data['images'] as $key => $value){
-                $res = PR_POST_Product_Image($id, $_SERVER['DOCUMENT_ROOT']."/uploads/images/products/".$value['image'], false);
-                $id_img_prestashop = (string) simplexml_load_string($res)->image->id;
-                $id_img = $value['id'];
-                $GLOBALS['DBCONN']->query(prefixQuery(/** @lang text */ "UPDATE {*product_images*} SET id_prestashop='$id_img_prestashop' WHERE id='$id_img'"));
-            }
-        }
-
-        PR_PUT_Product_Stock_Available($id, $data['quantity']);
-    }
+function PR_PUT_Product_Stock($id_product){
+    global $domain, $api_key;
+    $data = get_product($id_product);
+    $id = PR_GET_Product_By_Tag($data['tag'])['products'][0]['id'];
+    PR_PUT_Product_Stock_Available($id, $data['quantity']);
 }
 function PR_PUT_Product_Without_IMG($id_product){
     global $domain, $api_key;
     $data = get_product($id_product);
+    $codes = get_ean_codes($id_product);
+    $codes_xml = "";
+    if (!empty($codes)){
+        $codes_xml = "<ean13>".reset($codes)['ean']."</ean13>";
+    }
+    $pr = PR_GET_Product_By_Tag($data['tag']);
+    if (empty($pr)){
+        PR_POST_Product($id_product, true);
+    }
     $id = PR_GET_Product_By_Tag($data['tag'])['products'][0]['id'];
     $PriceTaxExcluded = round($data['platforms'][_ENGINE['ps_platform_id']]['price']/1.2, 5);
     $active = 0;
@@ -315,10 +416,17 @@ function PR_PUT_Product_Without_IMG($id_product){
     foreach ($data['categories'] as $value){
         $categories .= "<category><id>$value</id></category>";
     }
+    if (isset($data['id_manufacturer'])){
+        $manufacturer = '<id_manufacturer><![CDATA['.PR_GET_Manufacturer($data['id_manufacturer']).']]></id_manufacturer>';
+    } else {
+        $manufacturer = "";
+    }
     $xml = '
 <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
   <product>
     <id>'.$id.'</id>
+    '.$manufacturer.'
+    '.$codes_xml.'
     <name>
         <language id="'._LANG['ps_en'].'"><![CDATA['.trim($data['name']['en']).']]></language>
         <language id="'._LANG['ps_et'].'"><![CDATA['.trim($data['name']['et']).']]></language>
@@ -338,8 +446,10 @@ function PR_PUT_Product_Without_IMG($id_product){
     <price>'.$PriceTaxExcluded.'</price>
     <reference>'.$data['tag'].'</reference>
     <active>'.$active.'</active>
+   
     <show_price>1</show_price>
     <pack_stock_type>3</pack_stock_type>
+    <minimal_quantity>1</minimal_quantity>
     <state>1</state>
     <indexed>1</indexed>
     <id_category_default>'.$data['main_category'].'</id_category_default>
@@ -365,9 +475,40 @@ function PR_PUT_Product_Without_IMG($id_product){
     }
 }
 
+function PR_GET_Manufacturer($id_manufacturer){
+    global $domain, $api_key;
+    $manufacturer = preg_replace("/^\p{Z}+|\p{Z}+$/u", "", trim(get_manufacturer_name($id_manufacturer)));
+    $url_data = array(
+        'filter[name]' => $manufacturer,
+        'output_format' => 'JSON'
+    );
+    $url = "https://$api_key@$domain/api/manufacturers/?".http_build_query($url_data);
+    $res = CallGETAPI($url, true);
+    if (empty($res)){
+        $url = "https://$api_key@$domain/api/manufacturers/?output_format=JSON";
+        $xml = '
+            <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
+              <manufacturer>
+                <active><![CDATA[1]]></active>
+                <name><![CDATA['.$manufacturer.']]></name>
+              </manufacturer>
+            </prestashop>';
+        $res = CallPOSTAPI($url, $xml);
+        return $res['manufacturer']['id'];
+
+    } else {
+        return $res['manufacturers'][0]['id'];
+    }
+}
+
 function PR_PUT_Product_category_only($id_product){
     global $domain, $api_key;
     $data = get_product($id_product);
+    $codes = get_ean_codes($id_product);
+    $codes_xml = "";
+    if (!empty($codes)){
+        $codes_xml = "<ean13>".reset($codes)['ean']."</ean13>";
+    }
     $id = PR_GET_Product_By_Tag($data['tag'])['products'][0]['id'];
     $PriceTaxExcluded = round($data['platforms'][_ENGINE['ps_platform_id']]['price']/1.2, 5);
     $categories = "";
@@ -378,6 +519,7 @@ function PR_PUT_Product_category_only($id_product){
 <prestashop xmlns:xlink="http://www.w3.org/1999/xlink">
   <product>
     <id>'.$id.'</id>
+    '.$codes_xml.'
     <name>
         <language id="'._LANG['ps_en'].'"><![CDATA['.trim($data['name']['en']).']]></language>
         <language id="'._LANG['ps_et'].'"><![CDATA['.trim($data['name']['et']).']]></language>
@@ -392,6 +534,7 @@ function PR_PUT_Product_category_only($id_product){
     <reference>'.$data['tag'].'</reference>
     <active>1</active>
     <pack_stock_type>3</pack_stock_type>
+    <minimal_quantity>1</minimal_quantity>
     <state>1</state>
     <indexed>1</indexed>
     <id_category_default>'.$data['main_category'].'</id_category_default>
